ecbba1967421f6cc4fbd14dcab978c1d749acbed
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2020-09-26 13:21:56 +0100

    :construction_worker: Update deployment scripts

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,28 +1,21 @@
 const execa = require("execa");
-const emoji = require("node-emoji");
-const chalk = require("chalk");
 const fs = require("fs");
-
-const e_arrows = emoji.get("fast_forward");
-const e_rocket = emoji.get("rocket");
 
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    console.log(`${e_arrows} ${chalk.yellow("Building...")}`);
+    console.log("Building...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log(`${e_arrows} ${chalk.yellow("Pushing...")}`);
+    console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log(
-      `${e_rocket} ${chalk.green("Successfully deployed")} ${e_rocket}`
-    );
+    console.log("Successfully deployed");
   } catch (e) {
     console.log(e.message);
     process.exit(1);
